f410dc81696e9de6702291aa60ee8a3a039cdaa5
jam-hur <117577159+jam-hur@users.noreply.github.com>
2023-01-15 10:14:57 +0800

    Add files via upload
    Initial upload of website files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function buttonClick () {
+    document.getElementById("demo").innerHTML = 'You clicked it bitches!';
+    document.getElementById("button").innerHTML = 'Click me again bitches!';
+    
+}
+
